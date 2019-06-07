6cebbe94e97ae819c3cac0bb0c688b0fa42816f3
Mika Mannermaa <mmannerm@gmail.com>
2019-06-06 21:07:11 -0700

    In MacOS install ansible via homebrew python3. In Linux, use pip.

--- osx_vagrant.rb
+++ osx_vagrant.rb
@@ -2,6 +2,7 @@ Vagrant.configure(2) do |config|
 	config.vm.provision "shell", privileged: false, inline: <<-SHELL
                 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
                 brew install python
+		pip3 install ansible
 	SHELL
 end
 
